6b3ff3e671c662805a76ffdbcf8779d9efc4d28e
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2016-02-22 22:28:56 -0800

    [demo]

---
+++ CompressionDemo/7zipcompression/7zipCompress.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace _7zipCompression
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string sourceName = args[0];
+			string targetName = args[1];
+		//	if (File.Exists(sourceName))
+		//		Process.Start(sourceName);
+			// 1
+			// Initialize process information.
+			//
+			ProcessStartInfo p = new ProcessStartInfo();
+			p.FileName = "7zG.exe";
+
+			// 2
+			// Use 7-zip
+			// specify a=archive and -tgzip=gzip
+			// and then target file in quotes followed by source file in quotes
+			//
+			p.Arguments = "a -tgzip \"" + targetName + "\" \"" +
+				sourceName + "\" -mx=9";
+			p.WindowStyle = ProcessWindowStyle.Normal;
+
+			// 3.
+			// Start process and wait for it to exit
+			//
+			Process x = Process.Start(p);
+			x.WaitForExit();
+		}
+	}
+}
---
+++ CompressionDemo/CompressionDemo/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace CompressionDemo
+{
+    //https://msdn.microsoft.com/library/dd576348(v=vs.100).aspx
+    class Program
+    {
+        static void Main( string[] args )
+        {
+        }
+    }
+
+    class gZip
+    {
+        public static void DirCompress( DirectoryInfo directorySelected, string filter )
+        {
+            foreach( FileInfo fileToCompress in directorySelected.GetFiles() )
+            { 
+                using(FileStream originalFileStream = fileToCompress.OpenRead())
+                {
+                    if((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) != FileAttributes.Hidden) && fileToCompress.Extension != filter)
+                    {
+                        using (FileStream CompressedFileStream = File.Create(fileToCompress.FullName + ".gz"))
+                        {
+                            using (GZipStream compressionStream = new GZipStream( CompressedFileStream, CompressionMode.Compress))
+                            {
+                                originalFileStream.CopyTo(compressionStream);
+                            }
+                        }
+                    }
+                } 
+            }
+        }
+    }
+
+    class Zip
+    {
+        static void SimpleZip( string dirToZip, string zipName )
+        {
+            ZipFile.CreateFromDirectory(dirToZip, zipName);
+        }
+
+        static void SimpleZip(string dirToZip, string zipName, CompressionLevel compressLvl, bool includeRoot)
+        {
+            ZipFile.CreateFromDirectory( dirToZip, zipName, compressLvl, includeRoot );
+        
+        }
+        static void SimpleUnzip( string zipName, string dir )
+        {
+            ZipFile.ExtractToDirectory( zipName, dir );
+        }
+        static voi SmartUnzip( string name, string dir )
+        {
+            string fileUnzipFullPath;
+            string fileUnzipFullName;
+
+            using( ZipArchive archive = ZipFile.OpenRead( name ) )
+            {
+                foreach( ZipArchiveEntry file in archive.Entries )
+                {
+                    Console.WriteLine( "File Name: {0}", file.Name );
+                    Console.WriteLine( "File Size: {0} Bytes", file.Length );
+                    Console.WriteLine( "Compression Ratio: {0}");
+
+                    fileUnzipFullName = Path.Combine( dir, file.FullName );
+                    if( !System.IO.File.Exists( fileUnzipFullName ) )
+                    {
+                        fileUnzipFullPath = Path.GetDirectoryName( fileUnzipFullName );
+                        Directory.CreateDirectory( fileUnzipFullPath );
+                        file.ExtractToFile( fileUnzipFullName)
+                    
+                    }
+
+                
+                }
+            
+            }
+        }
+
+        public enum Overwrite
+        {
+            Always,
+            IfNewer,
+            Never
+        }
+        public enum ArchiveAction
+        {
+            Merge,
+            Replace,
+            Error,
+            Ignore
+        }
+        static void improvedExtractToFile( ZipArchiveEntry file, string destPath, Overwrite owMethod = Overwrite.IfNewer )
+        {
+            string destFilename = Path.Combine( destPath, file.FullName );
+            string destFilePath = Path.GetDirectoryName( destFilename );
+            Directory.CreateDirectory( destFilePath );
+            switch( owMethod )
+            { 
+            case Overwrite.Always:
+            file.ExtractToFile( destinationFileName, true );
+            break;
+            case Overwrite.IfNewer:
+            if( !File.Exists( destFilename ) || File.GetLastWriteTime(destFilename) < file.LastWriteTime)
+            {
+                file.ExtractToFile( destFilename, true );
+            }
+            break;
+            case Overwrite.Never:
+            if( !File.Exists( destFilename ) )
+            {
+                file.ExtractToFile( destinationFileName );  
+            }
+            break;
+            default:
+            break;
+            
+            }
+        }
+    }
+}
